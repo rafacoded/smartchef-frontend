301feefed04e2d70c327267961a5139b8bb4cd7a
rafa <rafalintirado50@gmail.com>
2026-01-27 18:16:54 +0100

    Cambios en package.json y capacitor.config.ts para AppFlow y la importación del proyecto

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'smartchef',
+  appId: 'com.rafaa.smartchef',
+  appName: 'SmartChef',
   webDir: 'www'
 };
 
